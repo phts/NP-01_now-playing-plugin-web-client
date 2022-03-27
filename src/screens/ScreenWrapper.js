524bc04f324965881cae23cdf28c11bb4e8b5fac
patrickkfkan <patrickkfkan@gmail.com>
2022-03-27 22:04:01 +0800

    Dim bg instead blur if transitions  disabled

--- src/screens/ScreenWrapper.js
+++ src/screens/ScreenWrapper.js
@@ -1,16 +1,18 @@
 import ContextualCSSTransition from "../common/ContextualCSSTransition";
 import { useModals } from "../contexts/ModalStateProvider";
+import { usePerformanceSettings } from "../contexts/SettingsProvider";
 import { ACTION_PANEL } from "../modals/CommonModals";
 import './ScreenWrapper.scss';
 
 const ScreenWrapper = ({ children }) => {
 
   const {isModalOpened} = useModals();
+  const {disableTransitions} = usePerformanceSettings();
 
   return (
     <ContextualCSSTransition
       in={isModalOpened(ACTION_PANEL)}
-      classNames="ScreenWrapper--blur"
+      classNames={!disableTransitions ? "ScreenWrapper--blur" : "ScreenWrapper--dim"}
       timeout={100}>
         <div className="ScreenWrapper">
           {children}
