b8bdc5e911dd51c82b303802c0972aa67e949b01
patrickkfkan <patrickkfkan@gmail.com>
2022-05-07 16:39:25 +0800

    Add hourly weather to idle screen

--- src/screens/idle/IdleScreenWeather.js
+++ src/screens/idle/IdleScreenWeather.js
@@ -5,6 +5,10 @@ import { useLocale, useRawSettings, useTimezone } from '../../contexts/SettingsP
 import { DateTime } from 'luxon';
 import { useTranslation } from 'react-i18next';
 import Scrollbars from 'rc-scrollbars';
+import { useCallback, useEffect, useState } from 'react';
+import { useStore } from '../../contexts/StoreProvider';
+
+const RESTORE_STATE_KEY = 'IdleScreen.Weather.restoreState';
 
 const getBackgroundStyles = (settings) => {
   const weatherBackgroundType = settings.weatherBackground || 'default';
@@ -30,6 +34,9 @@ const getBackgroundStyles = (settings) => {
 
 function IdleScreenWeather() {
   const {t} = useTranslation();
+  const store = useStore();
+  const restoreState = store.get(RESTORE_STATE_KEY, {}, true);
+  const [view, setView] = useState(restoreState.view || 'day');
   const {settings: screenSettings} = useRawSettings('screen.idle');
   const customFontSizes = screenSettings.fontSizes === 'custom';
   const customFontColors = screenSettings.fontColors === 'custom';
@@ -37,15 +44,15 @@ function IdleScreenWeather() {
   const weatherCurrentColor = customFontColors && screenSettings.weatherCurrentColor ? screenSettings.weatherCurrentColor : null;
   const backgroundStyles = getBackgroundStyles(screenSettings);
   const currentWeatherStyles = {
-    '--day-base-font-size': customFontSizes && screenSettings.weatherCurrentBaseFontSize ? screenSettings.weatherCurrentBaseFontSize : null,
-    '--day-title-color': weatherCurrentColor,
+    '--period-base-font-size': customFontSizes && screenSettings.weatherCurrentBaseFontSize ? screenSettings.weatherCurrentBaseFontSize : null,
+    '--period-title-color': weatherCurrentColor,
     '--temp-range-color': weatherCurrentColor,
     '--current-temp-color': weatherCurrentColor,
     '--icon-size': customIconSettings && screenSettings.weatherCurrentIconSize ? screenSettings.weatherCurrentIconSize : null
   };
   const forecastWeatherStyles = {
-    '--day-base-font-size': customFontSizes && screenSettings.weatherForecastBaseFontSize ? screenSettings.weatherForecastBaseFontSize : null,
-    '--day-title-color': customFontColors && screenSettings.weatherForecastColor ? screenSettings.weatherForecastColor : null,
+    '--period-base-font-size': customFontSizes && screenSettings.weatherForecastBaseFontSize ? screenSettings.weatherForecastBaseFontSize : null,
+    '--period-title-color': customFontColors && screenSettings.weatherForecastColor ? screenSettings.weatherForecastColor : null,
     '--temp-range-color': customFontColors && screenSettings.weatherForecastColor ? screenSettings.weatherForecastColor : null,
     '--icon-size': customIconSettings && screenSettings.weatherForecastIconSize ? screenSettings.weatherForecastIconSize : null
   };
@@ -63,10 +70,32 @@ function IdleScreenWeather() {
     weekday: 'short'
   };
 
+  const TIME_FORMAT = {
+    ...DateTime.TIME_SIMPLE,
+    hour12: (screenSettings.timeFormat === undefined || 
+      screenSettings.timeFormat === 'default' || !screenSettings.hour24) ? true : false
+  };
+
+  const DATE_FORMAT = {
+    ...DateTime.DATE_SHORT,
+    year: undefined
+  };
+
+  useEffect(() => {
+    restoreState.view = view;
+  }, [restoreState, view]);
+
   const getDayName = (millis) => {
     return DateTime
       .fromMillis(millis, {zone: timeZone, locale})
       .toLocaleString(DAY_FORMAT);
+  };
+
+  const getTimeOrDateString = (millis) => {
+    const dt = DateTime.fromMillis(millis, {zone: timeZone, locale});
+    const isMidnight = dt.hour === 0 && dt.minute === 0;
+    const format = isMidnight ? DATE_FORMAT : TIME_FORMAT;
+    return dt.toLocaleString(format);
   };
 
   const getIcon = (data, infoType, baseClassName, iconMonoColor, animate = false) => {
@@ -95,20 +124,24 @@ function IdleScreenWeather() {
 
   const getCurrentBlock = (data) => {
     return (
-      <div className={styles.Current} style={currentWeatherStyles}>
+      <div className={styles.Current} style={currentWeatherStyles} onClick={toggleView}>
         {getIcon(data, 'condition', 'Current__icon', iconSettings.currentMonoColor, iconSettings.currentIconAnimate)}
         <div className={styles.Current__info}>
-          <div className={styles.Current__dayTitleWrapper}>
-            <span className={styles.Current__dayTitle}>
-              {t('weather.today')}
+          <div className={styles.Current__periodTitleWrapper}>
+            <span className={styles.Current__periodTitle}>
+              {view === 'day' ? t('weather.today') : t('weather.now')}
             </span>
           </div>
           <span className={styles.Current__currentTemp}>
             {data.temp.now.text}
           </span>
-          <span className={styles.Current__tempRange}>
-            {data.temp.min.shortText} / {data.temp.max.shortText}
-          </span>
+          {
+            view === 'day' ? 
+              <span className={styles.Current__tempRange}>
+                {data.temp.min.shortText} / {data.temp.max.shortText}
+              </span>
+            : null
+          }          
         </div>
       </div>
     )
@@ -117,19 +150,69 @@ function IdleScreenWeather() {
   const getForecastBlock = (data, key) => {
     return (
       <div key={key} className={styles.Forecast} style={forecastWeatherStyles}>
-        <div className={styles.Forecast__dayTitleWrapper}>
-          <span className={styles.Forecast__dayTitle}>
-            {getDayName(data.dateTimeMillis)}
+        <div className={styles.Forecast__periodTitleWrapper}>
+          <span className={styles.Forecast__periodTitle}>
+            {
+              view === 'day' ? getDayName(data.dateTimeMillis) : getTimeOrDateString(data.dateTimeMillis)
+            }
           </span>
         </div>
         {getIcon(data, 'condition', 'Forecast__icon', iconSettings.forecastMonoColor)}
         <div className={styles.Forecast__info}>
-          <span className={styles.Forecast__tempRange}>
-            {data.temp.min.shortText} / {data.temp.max.shortText}
-          </span>
+          {
+            view === 'day' ?
+              <span className={styles.Forecast__tempRange}>
+                {data.temp.min.shortText} / {data.temp.max.shortText}
+              </span>
+            :
+              <span className={styles.Forecast__temp}>
+                {data.temp.shortText}
+              </span>
+          }
         </div>
       </div>
     )
+  };
+
+  const toggleView = useCallback((e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setView(view === 'day' ? 'hour' : 'day');
+  }, [view]);
+
+  const getDayViewContents = (weatherInfo) => {
+    return (
+      <div className={styles.Contents}>
+        <div className={styles.Contents__currentWrapper}>
+          {getCurrentBlock(weatherInfo.current)}
+        </div>
+        <div className={styles.Contents__forecastWrapper}>
+          {
+            weatherInfo.forecast.map((data, index) => 
+              getForecastBlock(data, `forecast_${index}`)
+            )
+          }
+        </div>
+      </div>
+    );
+  };
+
+  const getHourViewContents = (weatherInfo) => {
+    const currentTime = (new Date()).getTime();
+    return (
+      <div className={styles.Contents}>
+        <div className={styles.Contents__currentWrapper}>
+          {getCurrentBlock(weatherInfo.current)}
+        </div>
+        <div className={styles.Contents__forecastWrapper}>
+          {
+            weatherInfo.hourly
+              .filter(data => data.dateTimeMillis > currentTime)
+              .map((data, index) => getForecastBlock(data, `forecast_${index}`))
+          }
+        </div>
+      </div>
+    );
   };
 
   if (weather.status === 'fetched') {
@@ -141,16 +224,9 @@ function IdleScreenWeather() {
             thumbHorizontal: 'Scrollbar__handle'
           }}
           autoHide>
-            <div className={styles.DayView}>
-              <div className={styles.DayView__currentWrapper}>
-                {getCurrentBlock(info.current)}
-              </div>
-              <div className={styles.DayView__forecastWrapper}>
-                {info.forecast.map((data, index) => 
-                  getForecastBlock(data, `forecast_${index}`)
-                )}
-              </div>
-            </div>
+            {
+              view === 'day' ? getDayViewContents(info) : getHourViewContents(info)
+            }
         </Scrollbars>
       </div>
     );
