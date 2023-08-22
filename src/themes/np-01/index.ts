484ff4f31ddc3e1743a36218c57db26d6d8b4f62
Phil Tsaryk <phil@tsaryk.com>
2023-08-22 22:03:36 +0200

    chore: duplicate default theme

---
+++ src/themes/np-01/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="../../declaration.d.ts" />
+
+import { Theme } from '../../contexts/settings/ThemeProvider';
+import styles from './styles.module.scss';
+
+const theme: Theme = {
+  name: 'np-01',
+  className: styles.DefaultTheme,
+};
+
+export default theme;
